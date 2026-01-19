40e8babe66524c3987a3f458a2b6a245d96294ce
Matheo Bocquet <matheo.bocquet18@gmail.com>
2026-01-19 14:04:19 +0100

    implémentation de l'authentification JWT avec la configuration de Spring Security, création de l'entité User et de son repository UserRepository, ajout de JwtUtils pour la génération et validation des tokens, mise en place du JwtFilter pour la sécurisation des requêtes et création du controller AuthController avec les routes /register et /login

---
+++ src/main/java/com/example/carnetadresses/config/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.carnetadresses.config;
+
+import com.example.carnetadresses.model.User;
+import com.example.carnetadresses.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé : " + username));
+
+                return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().replace("ROLE_", ""))
+                .build();
+    }
+}
---
+++ src/main/java/com/example/carnetadresses/model/User.java
@@ -0,0 +1,21 @@
+package com.example.carnetadresses.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Getter @Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role; //ROLE_USER , ROLE_ADMIN
+}

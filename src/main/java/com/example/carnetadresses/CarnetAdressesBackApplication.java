1741911601a655346532539c8fff139c4b982589
Matheo Bocquet <matheo.bocquet18@gmail.com>
2026-01-19 13:13:00 +0100

    Initialisation du projet, avec l'installation des dépendances pour le backend sous Springboot et création de la base de donnée MySQL

---
+++ src/main/java/com/example/carnetadresses/CarnetAdressesBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.carnetadresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetAdressesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetAdressesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carnetadresses/CarnetAdressesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carnetadresses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetAdressesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
